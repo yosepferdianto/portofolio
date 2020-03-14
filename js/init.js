99118037cb829374394b33a87d159c607bf1a723
yosep <i.sef07@gmail.com>
2020-03-15 02:07:51 +0700

    Remastered

--- js/init.js
+++ js/init.js
@@ -538,7 +538,7 @@ function arlo_tm_animate_text() {
 	var animateSpan = jQuery('.arlo_tm_animation_text_word');
 
 	animateSpan.typed({
-		strings: ["IT Backend", "Programmer", "Web Developer"],
+		strings: ["from Indonesia", "a Programmer", "a Web Developer"],
 		loop: true,
 		startDelay: 1e3,
 		backDelay: 2e3
